a6e54894d035aae680a37852552213c4912d09b7
lakhwinder555 <lakhwindersinghwins@gmail.com>
2025-01-07 16:25:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+
+// Use the port provided by Azure, or fallback to 5000 for local development
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
